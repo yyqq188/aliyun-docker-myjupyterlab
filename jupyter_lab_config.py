be2615db5367d24c9ee7cb4c13f46d33e1e29207
ubuntu <ubuntu@localhost.localdomain>
2022-03-12 12:58:46 +0800

    change2

--- jupyter_lab_config.py
+++ jupyter_lab_config.py
@@ -1,9 +1,8 @@
 c.ServerApp.password = ''
 
-c.ServerApp.certfile = u'/root/.jupyter/mycert.pem'
-c.ServerApp.keyfile = u'/root/.jupyter/mykey.key'
+#c.ServerApp.certfile = u'/root/.jupyter/mycert.pem'
+#c.ServerApp.keyfile = u'/root/.jupyter/mykey.key'
 c.ServerApp.ip = '0.0.0.0'
 c.ServerApp.port = 8888
 c.ServerApp.open_browser = False
 c.ServerApp.allow_root = True
-c.ServerApp.token = ''
